0c10f868a7c3ca4e1e1e8f5488b6b2ad112be5ff
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-08-04 16:24:57 +0800

    add profile content

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import { NavBar,Button,Modal } from 'antd-mobile';
+import React, {Component} from 'react';
+import {NavBar, List, Button, Modal} from 'antd-mobile';
+
 const alert = Modal.alert;
+const Item = List.Item;
+const Brief = Item.Brief;
+
 class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }
+        this.state = {}
     }
-    logout(){
+
+    logout() {
         alert('确认退出?', '', [
-            { text: '取消', onPress: () => console.log('cancel') },
-            { text: '确认', onPress: () =>{
-                localStorage.removeItem("access_token")
-                this.props.history.push("/login")}  
+            {text: '取消', onPress: () => console.log('cancel')},
+            {
+                text: '确认', onPress: () => {
+                    localStorage.removeItem("access_token")
+                    this.props.history.push("/login")
+                }
             },
-          ])
+        ])
     }
+
     render() {
         return (
-            <div type="flex" >
+            <div type="flex">
                 <NavBar mode="dark">个人</NavBar>
-                <Button style={{position: 'fixed', bottom: 55,width:"100%"  }} 
-                        type="primary" 
-                        onClick={() => { this.logout() }}>退出登录</Button>
+                <Item
+
+                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                    multipleLine
+                >
+                    用户名:<Brief></Brief>
+                </Item>
+                <Item
+
+                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                    multipleLine
+                >
+                    邮箱:<Brief></Brief>
+                </Item>
+                <Item
+
+                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                    multipleLine
+                >
+                    手机:<Brief></Brief>
+                </Item>
+                <Item
+
+                    arrow="horizontal"
+                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                    multipleLine
+                    extra="详情"
+                    onClick={() => {
+                    }}
+                >
+                    停车场<Brief></Brief>
+
+                </Item>
+                <Button style={{position: 'fixed', bottom: 55, width: "100%"}}
+                        type="primary"
+                        onClick={() => {
+                            this.logout()
+                        }}>退出登录</Button>
             </div>
         );
     }
