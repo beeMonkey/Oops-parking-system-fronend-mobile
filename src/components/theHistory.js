13729981e96aef98e801716f7b314107ae61c845
louis <louis.z.j.li@oocl.com>
2018-08-04 21:34:27 +0800

    [louis] history

--- src/components/theHistory.js
+++ src/components/theHistory.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { NavBar, List } from 'antd-mobile';
+import { NavBar, List, Result } from 'antd-mobile';
 const Item = List.Item;
 const Brief = Item.Brief;
+const myImg = src => <img src={src} style={{ width: "2rem", height: "2rem" }} alt="" />;
 class TheHistory extends Component {
     constructor(props) {
         super(props);
@@ -18,21 +19,26 @@ class TheHistory extends Component {
                 <NavBar
                     mode="dark"
                 >历史</NavBar>
-                {items.length>0 &&
-                <List className="my-list">
-                    {items.map(item => {
-                        return <Item
-                            thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
-                            multipleLine
-                        >
-                            订单{item.id} <Brief>{item.carId}</Brief>
-                        </Item>
-                    })}
-                </List>
+                {items.length > 0 &&
+                    <List className="my-list">
+                        {items.map(item => {
+                            return <Item
+                                thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+                                multipleLine
+                            >
+                                订单{item.id} <Brief>{item.carId}</Brief>
+                            </Item>
+                        })}
+                    </List>
                 }
-                {items.length===0 &&
-                <p style={{marginTop:"60%",textAlign:"center",fontSize:"2rem"}}>暂无历史订单</p>
+                {items.length === 0 &&
+                    <Result
+                        style={{ marginTop: "60%", textAlign: "center", fontSize: "1.5rem"}}
+                        img={myImg('https://gw.alipayobjects.com/zos/rmsportal/GIyMDJnuqmcqPLpHCSkj.svg')}
+                        title="您的业绩不太好，还要加强喔！"
+                    />
                 }
+
             </div>
         );
     }
