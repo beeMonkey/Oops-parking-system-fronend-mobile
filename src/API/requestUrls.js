588bbd366142c4e98315b56add18d88e832e6989
louis <louis.z.j.li@oocl.com>
2018-08-05 10:40:41 +0800

    [louis] set request url to romte url

--- src/API/requestUrls.js
+++ src/API/requestUrls.js
@@ -1,5 +1,5 @@
-// const globalUrl = "https://parkinglot-backend.herokuapp.com/api/v1/" //dev
-const globalUrl = "http://10.222.232.147:8080/api/v1/" //test
+const globalUrl = "https://parkinglot-backend.herokuapp.com/api/v1/" //dev
+// const globalUrl = "http://10.222.232.147:8080/api/v1/" //test
 export default {
     "employees":globalUrl + "users",
     "login":globalUrl+"login",
